bc8b9bcae15830970bcae7bd2e005d36698f2d16
Keith Yong <terda12@gmail.com>
2015-06-15 16:54:05 -0400

    Added contacts h2

--- js/About.jsx
+++ js/About.jsx
@@ -10,6 +10,7 @@ module.exports = React.createClass({
                 <h2>Hello!</h2>
                 <p>My name is Keith. I am a senior Computer Science student at the <a href="http://www.udel.edu/">University of Delaware</a>. I focus on front-end web development using <a href="https://facebook.github.io/react/">React.js</a>, and I also do full stack work on <a href="https://nodejs.org/">Node</a> and <a href="http://flask.pocoo.org/">Flask</a>. My database of choice is PostgreSQL.</p>
                 <p>In my free time you can find me buying a bag of Stumptown coffee in NYC, painting a still life oil, or playing some Dota 2.</p>
+                <h2>Contacts</h2>
                 <p>You can email me at <a href="mailto:terda12@gmail.com">terda12@gmail.com</a> and download my resume <a href="resume.pdf">here</a>.</p>
             </div>
         )
