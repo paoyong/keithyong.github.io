1c7379fd75b30d10b22f8e353aa38b6e6d6cfc3d
Keith Yong <terda12@gmail.com>
2015-06-26 11:35:31 -0400

    Fixed avatar location link

--- src/js/About.jsx
+++ src/js/About.jsx
@@ -6,7 +6,7 @@ module.exports = React.createClass({
     render: function() {
         return (
             <div className="row about">
-                <img src="images/avatar.png" id="avatar" alt="avatar"></img>
+                <img src="src/images/avatar.png" id="avatar" alt="avatar"></img>
                 <h2>Hello!</h2>
                 <p>My name is Keith. I am a senior Computer Science student at the <a href="http://www.udel.edu/">University of Delaware</a>. I focus on front-end web development using <a href="https://facebook.github.io/react/">React.js</a>, and I also do full stack work on <a href="https://nodejs.org/">Node</a> and <a href="http://flask.pocoo.org/">Flask</a>. My database of choice is <a href="http://www.postgresql.org/">PostgreSQL</a>.</p>
                 <p>In my free time you can find me buying a bag of <a href="https://www.stumptowncoffee.com/">Stumptown</a> coffee in NYC, painting a still life oil, or playing some <a href="https://www.dota2.com/reborn/part1/">Dota 2</a>.</p>
