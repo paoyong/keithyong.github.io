ca0b9d98c390b9f7346f62582354bd18c668b929
Keith Yong <terda12@gmail.com>
2015-09-02 07:26:45 -0400

    Improved padding on the footer container and content in mobile.

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -28,7 +28,6 @@ var App = React.createClass({
                                 <li><Link to="about">About</Link></li>
                                 <li><Link to="travel">Travel</Link></li>
                                 <li><a href="https://github.com/keithyong">GitHub</a></li>
-                                <li><a href="resume/resume.pdf">Resume</a></li>
                             </ul>
                         </div>
                     </nav>
